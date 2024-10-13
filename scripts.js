7f383c601e16a113f226feb17f64d671a34d9fff
Rafael Machado <rafael@rafarodrigues.com>
2024-10-12 22:15:55 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,36 +11,27 @@ function wrapWords(selector) {
   });
 }
 
-// Selecionar todos os elementos com o custom attribute data-animate="true"
-const animatedElements = document.querySelectorAll('[data-animate="true"]');
+// Aplicar animações para elementos com as classes específicas
+wrapWords(".blur-text, .fade-in");
 
-// Certifique-se de que encontrou elementos antes de aplicar animações
-if (animatedElements.length > 0) {
-  // Envolver as palavras em <span>
-  wrapWords('[data-animate="true"]');
+gsap.utils.toArray(".blur-text .word").forEach((element) => {
+  gsap.fromTo(element, 
+    { filter: "blur(10px)", opacity: 0 }, 
+    { filter: "blur(0px)", opacity: 1, duration: 1.1, stagger: 0.1, scrollTrigger: {
+      trigger: element,
+      start: "top 50%", 
+      toggleActions: "play none none none"
+    }}
+  );
+});
 
-  // Animação de blur e fade-in para palavras de elementos com data-animate="true"
-  animatedElements.forEach((element) => {
-    const words = element.querySelectorAll('.word');
-
-    // Animação de blur
-    gsap.fromTo(words, 
-      { filter: "blur(10px)", opacity: 0 }, 
-      { filter: "blur(0px)", opacity: 1, duration: 1.1, stagger: 0.1, scrollTrigger: {
-        trigger: element,
-        start: "top 50%", 
-        toggleActions: "play none none none"
-      }}
-    );
-
-    // Animação de fade-in
-    gsap.fromTo(words, 
-      { opacity: 0, y: 50 }, 
-      { opacity: 1, y: 0, duration: 1.1, stagger: 0.1, scrollTrigger: {
-        trigger: element,
-        start: "top 50%",
-        toggleActions: "play none none none"
-      }}
-    );
-  });
-}
+gsap.utils.toArray(".fade-in .word").forEach((element) => {
+  gsap.fromTo(element, 
+    { opacity: 0, y: 50 }, 
+    { opacity: 1, y: 0, duration: 1.1, stagger: 0.1, scrollTrigger: {
+      trigger: element,
+      start: "top 50%", 
+      toggleActions: "play none none none"
+    }}
+  );
+});
