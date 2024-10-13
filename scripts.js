9d57057c15745516d68c96e9b06350f6a8a8fad9
Rafael Machado <rafael@rafarodrigues.com>
2024-10-12 21:59:13 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,17 +15,17 @@ function wrapWords(selector) {
 wrapWords(".blur-text, .fade-in");
 
 gsap.utils.toArray(".blur-text .word").forEach((element) => {
-  gsap.fromTo(element, { filter: "blur(10px)", opacity: 0 }, { filter: "blur(0px)", opacity: 1, duration: 1.5, stagger: 0.1, scrollTrigger: {
+  gsap.fromTo(element, { filter: "blur(10px)", opacity: 0 }, { filter: "blur(0px)", opacity: 1, duration: 1.1, stagger: 0.1, scrollTrigger: {
     trigger: element,
-    start: "top 80%", 
+    start: "top 50%", 
     toggleActions: "play none none none"
   }});
 });
 
 gsap.utils.toArray(".fade-in .word").forEach((element) => {
-  gsap.fromTo(element, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1.5, stagger: 0.1, scrollTrigger: {
+  gsap.fromTo(element, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1.1, stagger: 0.1, scrollTrigger: {
     trigger: element,
-    start: "top 80%",
+    start: "top 50%",
     toggleActions: "play none none none"
   }});
 });
